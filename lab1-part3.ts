5777347e92fb111744b87304a9e7bcc2333dc255
Luis Naranjo <luisnaranjo733@gmail.com>
2017-01-08 14:04:12 -0800

    add some more this variables

--- lab1-part3.ts
+++ lab1-part3.ts
@@ -1,8 +1,10 @@
 class Account {
      balance: number;
+     owner: string;
 
-    constructor(initialBalance: number) {
+    constructor(initialBalance: number, owner: string) {
         this.balance = initialBalance;
+        this.owner = owner;
     }
 
     deposit(amount: number) {
@@ -12,24 +14,24 @@ class Account {
         this.balance = this.balance - amount;
     }
     checkBalance() {
-        console.log(`Account balance: $${this.balance}`);
+        console.log(`${this.owner} account balance: $${this.balance}`);
     }   
 }
 
 class CheckingsAccount extends Account {
-    constructor(initialBalance: number) {
-        super(initialBalance);
+    constructor(initialBalance: number, owner: string) {
+        super(initialBalance, owner);
     }
 }
 
 class SavingsAccount extends Account {
-    constructor(initialBalance: number) {
-        super(initialBalance);
+    constructor(initialBalance: number, owner: string) {
+        super(initialBalance, owner);
     }
 }
 
 console.log('Checkings account')
-let checkingsAccount = new CheckingsAccount(20);
+let checkingsAccount = new CheckingsAccount(20, "Luis");
 checkingsAccount.checkBalance();
 checkingsAccount.deposit(40);
 checkingsAccount.checkBalance();
@@ -37,7 +39,7 @@ checkingsAccount.withdraw(30);
 checkingsAccount.checkBalance();
 
 console.log('\nSavings account')
-let savingsAccount = new SavingsAccount(20);
+let savingsAccount = new SavingsAccount(20, "Luis");
 savingsAccount.checkBalance();
 savingsAccount.deposit(10);
 savingsAccount.checkBalance();
