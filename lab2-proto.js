c917ff27513682d9aba3d20f52f1167d9cddb5ba
Luis Naranjo <luisnaranjo733@gmail.com>
2017-01-10 08:48:53 -0800

    add some starter code to proto walkthrough
    I felt that giving them just an empty file might make them feel a bit lost
    since they've never seen the functional style class syntax. I left my hdmi
    adapter at home so I might not be able to present, so I need them to have
    some starter syntax

--- lab2-proto.js
+++ lab2-proto.js
@@ -1,1 +1,33 @@
 // prototypal inheritance walkthrough code goes here
+
+'use strict';
+
+// create an object literal for account which automatically clones Object.prototype and extends it
+var Account = {
+  // notice the lack of a constructor 
+  deposit: function (amount) {
+    this.balance = this.balance + amount;
+  },
+  withdraw: function (amount) {
+    this.balance = this.balance - amount;
+  },
+  checkBalance: function (amount) {
+    console.log(`${this.owner} account balance: $${this.balance}`);
+  }
+}
+
+
+
+// create a new Account object by using account object as the proto
+let account = Object.create(Account);
+
+// give the account an owner and a balance (since we can't have a constructor with this syntax)
+account.owner = "Ethan";
+account.balance = 20;
+
+account.checkBalance();
+account.deposit(40);
+account.checkBalance();
+account.withdraw(30);
+account.checkBalance();
+
